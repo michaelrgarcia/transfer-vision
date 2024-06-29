b8a7a86c4eb966d4df78b5dfa2fc8e5f5ce05077
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-06-29 15:59:02 -0700

    Import helvetica neue font from bundled fonts/

---
+++ src/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+
+import helveticaNeueLightWoff from "./fonts/helveticaneuelight-webfont.woff";
+import helveticaNeueLightWoff2 from "./fonts/helveticaneuelight-webfont.woff2";
+//

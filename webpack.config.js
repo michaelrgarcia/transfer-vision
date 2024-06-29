a5be5b066b31d900ba8a52ce194a6556de23cb90
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-06-29 15:27:51 -0700

    Add fonts to asset processing

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,25 @@
-const path = require('path');
+/* eslint-disable no-dupe-keys */
+const path = require("path");
 
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: ["./src/index.js"],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'imgs/[name].[ext]',
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "imgs/[name].[ext]",
     // eslint-disable-next-line no-dupe-keys
-    assetModuleFilename: 'svgs/[name].[ext]',
+    assetModuleFilename: "svgs/[name].[ext]",
+    assetModuleFilename: "fonts/[name].[ext]",
   },
   module: {
     rules: [
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
       },
     ],
   },

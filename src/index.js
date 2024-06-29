314cfc803204b6e7340dc826e676f28b8b6650a4
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-06-29 16:31:39 -0700

    Delete helvetica black neue

--- src/index.js
+++ src/index.js
@@ -2,5 +2,3 @@
 
 import helveticaNeueLightWoff from "./fonts/helveticaneuelight-webfont.woff";
 import helveticaNeueLightWoff2 from "./fonts/helveticaneuelight-webfont.woff2";
-import helveticaNeueBlackWoff from "./fonts/helveticaneueblack-webfont.woff";
-import helveticaNeueBlackWoff2 from "./fonts/helveticaneueblack-webfont.woff2";

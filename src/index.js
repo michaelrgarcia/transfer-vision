ef691c98a7ef5bd75a2e6163e1ba30d2c0b233c8
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-06-29 17:22:47 -0700

    Import select arrow img

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@
 
 import helveticaNeueLightWoff from "./fonts/helveticaneuelight-webfont.woff";
 import helveticaNeueLightWoff2 from "./fonts/helveticaneuelight-webfont.woff2";
+import selectArrow from "./imgs/arrow-204-16.png";
+//

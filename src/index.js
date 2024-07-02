1cc2a24f4052e1d3ecd9a635a139ce6b602607d9
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-07-02 00:35:05 -0700

    Fix express.js CORS routing

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import helveticaNeueLightWoff from "./fonts/helveticaneuelight-webfont.woff";
 import helveticaNeueLightWoff2 from "./fonts/helveticaneuelight-webfont.woff2";
 import selectArrow from "./imgs/arrow-204-16.png";
 import helpBox from "./svgs/help-box.svg";
+import getFourYears from "./public/assistDataImport";
+
+const schoolList = document.getElementById("four-year");
+const majorList = document.getElementById("major");
+const classList = document.getElementById("class");
+
+const hey = await getFourYears();

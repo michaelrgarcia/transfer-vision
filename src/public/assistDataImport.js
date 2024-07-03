1c70f84583b13bfccef3f89da22359bf24281c17
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-07-02 22:56:18 -0700

    Separate DOM logic from API fetching logic

--- src/public/assistDataImport.js
+++ src/public/assistDataImport.js
@@ -1,40 +1,31 @@
-import { defaultOption, selectOption } from "./elementPresets";
-
-import {
-  loadingState,
-  reverseLoadingState,
-  showLoadingText,
-  hideLoadingText,
-} from "./cssTransitions";
-
-export default async function getFourYears(schoolList) {
-  const formRow = schoolList.parentElement;
-  const loadingText = formRow.querySelector(".loading");
-  const everythingElse = formRow.querySelectorAll(":not(.loading)");
-
+export async function getFourYears() {
   try {
     const endpoint = "https://classglance.onrender.com/schools/four-years";
-    loadingState(everythingElse);
-    showLoadingText(loadingText);
+
     const response = await fetch(endpoint);
     const data = await response.json();
-    hideLoadingText(loadingText);
-    reverseLoadingState(everythingElse);
     const dataArray = Object.values(data);
 
-    const select = schoolList;
-    const placeholder = defaultOption("school");
-
-    select.appendChild(placeholder);
-
-    dataArray.forEach((school) => {
-      if (school.id < 200) {
-        const option = selectOption(school.name, school.id);
-
-        select.appendChild(option);
-      }
-    });
+    return dataArray;
   } catch (error) {
     console.error("Error fetching schools:", error);
+
+    return null;
+  }
+}
+
+export async function getMajorData(receivingId) {
+  try {
+    const endpoint = `https://classglance.onrender.com/schools/major-data/${receivingId}/74`;
+
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    const dataArray = Object.values(data);
+
+    return dataArray;
+  } catch (error) {
+    console.error(`Error fetching majors for ${receivingId}:`, error);
+
+    return null;
   }
 }
---
+++ src/public/assistDataRender.js
@@ -0,0 +1,62 @@
+import { defaultOption, selectOption } from "./elementPresets";
+
+import {
+  loadingState,
+  reverseLoadingState,
+  showLoadingText,
+  hideLoadingText,
+} from "./cssTransitions";
+
+import { getFourYears, getMajorData } from "./assistDataImport";
+
+export async function renderFourYears(schoolList) {
+  const formRow = schoolList.parentElement;
+  const loadingText = formRow.querySelector(".loading");
+  const everythingElse = formRow.querySelectorAll(":not(.loading)");
+
+  loadingState(everythingElse);
+  showLoadingText(loadingText);
+
+  const fourYears = await getFourYears();
+
+  const select = schoolList;
+  const placeholder = defaultOption("school");
+
+  select.appendChild(placeholder);
+
+  fourYears.forEach((school) => {
+    if (school.id < 200) {
+      const option = selectOption(school.name, school.id);
+
+      select.appendChild(option);
+    }
+  });
+
+  hideLoadingText(loadingText);
+  reverseLoadingState(everythingElse);
+}
+
+export async function renderMajorData(majorList, receivingId) {
+  const formRow = majorList.parentElement;
+  const loadingText = formRow.querySelector(".loading");
+  const everythingElse = formRow.querySelectorAll(":not(.loading)");
+
+  loadingState(everythingElse);
+  showLoadingText(loadingText);
+
+  const majorData = await getMajorData(receivingId);
+
+  const select = majorList;
+  const placeholder = defaultOption("major");
+
+  select.appendChild(placeholder);
+
+  majorData.forEach((obj) => {
+    const option = selectOption(obj.major, obj.key);
+
+    select.appendChild(option);
+  });
+
+  hideLoadingText(loadingText);
+  reverseLoadingState(everythingElse);
+}

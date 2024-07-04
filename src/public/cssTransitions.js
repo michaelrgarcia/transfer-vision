2a6ff9403648ab8c8e289dfdcc03456adfe7bf54
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-07-03 22:46:46 -0700

    Add disabled attribute to submit btn

--- src/public/cssTransitions.js
+++ src/public/cssTransitions.js
@@ -1,37 +1,13 @@
 const dialog = document.querySelector("dialog");
 
-export function loadingState(nodeList) {
+export function showDialog() {
   dialog.showModal();
   dialog.style.opacity = 1;
-
-  nodeList.forEach((element) => {
-    const node = element;
-
-    node.style.opacity = 0.15;
-    node.disabled = true;
-    node.style.userSelect = "none";
-
-    if (node.style.cursor === "pointer") {
-      node.style.cursor = "not-allowed";
-    }
-  });
 }
 
-export function reverseLoadingState(nodeList) {
+export function closeDialog() {
   dialog.style.opacity = 0;
   dialog.close();
-
-  nodeList.forEach((element) => {
-    const node = element;
-
-    node.style.opacity = 1;
-    node.disabled = false;
-    node.style.userSelect = "auto";
-
-    if (node.style.cursor === "not-allowed") {
-      node.style.cursor = "pointer";
-    }
-  });
 }
 
 export function showLoadingText(loadingText) {
@@ -47,35 +23,71 @@ export function hideLoadingText(loadingText) {
   loading.style.display = "none";
 }
 
+export function loadingState(nodeList) {
+  nodeList.forEach((element) => {
+    const node = element;
+
+    node.style.opacity = 0.15;
+    node.disabled = true;
+    node.style.userSelect = "none";
+
+    if (node.style.cursor === "pointer") {
+      node.style.cursor = "not-allowed";
+    }
+  });
+}
+
+export function reverseLoadingState(nodeList) {
+  nodeList.forEach((element) => {
+    const node = element;
+
+    node.style.opacity = 1;
+    node.disabled = false;
+    node.style.userSelect = "auto";
+
+    if (node.style.cursor === "not-allowed") {
+      node.style.cursor = "pointer";
+    }
+  });
+}
+
 export function applyDisabledState(formRow) {
   const row = formRow;
+
   const imgBtn = row.querySelector("label > img");
   const select = row.querySelector("select");
+  const submit = row.querySelector(".submit");
 
-  row.id = "disabled";
+  row.setAttribute("id", "disabled");
 
   if (imgBtn && select) {
-    imgBtn.id = "disabled";
-    select.classList.add("disabled-input");
-  } else {
-    const submit = row.querySelector(".submit");
+    imgBtn.setAttribute("id", "disabled");
 
-    submit.id = "disabled";
+    select.classList.add("disabled-input");
+    select.disabled = true;
+  } else if (submit) {
+    submit.setAttribute("id", "disabled");
+    submit.disabled = true;
   }
 }
 
 export function removeDisabledState(formRow) {
   const row = formRow;
 
-  if (row.id === "disabled") {
-    row.removeAttribute("id");
-  }
+  const imgBtn = row.querySelector("label > img");
+  const select = row.querySelector("select");
 
-  row.forEach((node) => {
-    if (node.id === "disabled") {
-      node.removeAttribute("id");
-    } else if (node.classList.contains("disabled-input")) {
-      node.classList.remove("disabled-input");
-    }
-  });
+  row.removeAttribute("id");
+
+  if (imgBtn && select) {
+    imgBtn.removeAttribute("id");
+
+    select.classList.remove("disabled-input");
+    select.disabled = false;
+  } else {
+    const submit = row.querySelector(".submit");
+
+    submit.removeAttribute("id");
+    submit.disabled = false;
+  }
 }

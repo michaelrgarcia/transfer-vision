4c4b5449fb411fbb96e5e6588d274997c2047874
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-07-03 22:47:36 -0700

    Render major data after school selection

--- src/index.js
+++ src/index.js
@@ -7,17 +7,51 @@ import helpBox from "./svgs/help-box.svg";
 import closeBox from "./svgs/close-box.svg";
 
 import { renderFourYears, renderMajorData } from "./public/assistDataRender";
+import {
+  applyDisabledState,
+  removeDisabledState,
+} from "./public/cssTransitions";
 
-const schoolList = document.getElementById("four-year");
-const majorList = document.getElementById("major");
-const classList = document.getElementById("class");
+const selects = [
+  document.getElementById("four-year"),
+  document.getElementById("major"),
+  document.getElementById("class"),
+];
+
+const submit = document.querySelector(".submit");
 
 const dialog = document.querySelector("dialog");
 const closeDialog = document.querySelector(".close-dialog");
 
-renderFourYears(schoolList);
-// renderMajorData(majorList, 79);
+document.addEventListener("DOMContentLoaded", () => {
+  for (let i = 1; i < selects.length; ) {
+    if (selects[i].parentNode) {
+      applyDisabledState(selects[i].parentNode);
+      i += 1;
+    }
+  }
+
+  applyDisabledState(submit);
+
+  renderFourYears(selects[0]);
+});
+
+selects[0].addEventListener("input", () => {
+  const schoolList = selects[0];
+  const majorList = selects[1];
+
+  const selectedOption = schoolList.options[schoolList.selectedIndex];
+  const receivingId = selectedOption.dataset.sending;
+
+  removeDisabledState(schoolList.parentNode);
+
+  renderMajorData(majorList, receivingId);
+
+  removeDisabledState(majorList.parentNode);
+});
 
 closeDialog.addEventListener("click", () => {
   dialog.close();
 });
+
+// save school data in sessionStorage? (maybe all data?)

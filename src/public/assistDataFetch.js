8f01d457a8d12210bb2740b865b7e6220ce669a9
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-07-11 01:46:03 -0700

    Create getAllMajorArtics. and getCCCIds

--- src/public/assistDataFetch.js
+++ src/public/assistDataFetch.js
@@ -14,6 +14,46 @@ export async function getFourYears() {
   }
 }
 
+export async function getCCCIds() {
+  try {
+    const endpoint =
+      "https://classglance.onrender.com/schools/community-colleges";
+
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    const dataArray = Object.values(data);
+
+    const idArray = [];
+
+    dataArray.forEach((college) => {
+      if (college.id) {
+        idArray.push(college.id);
+      }
+    });
+
+    return idArray;
+  } catch (error) {
+    console.error("Error fetching schools:", error);
+
+    return null;
+  }
+}
+
+async function getArticulationParams(receivingId, majorKey) {
+  const articulationParams = [];
+  const cccIds = await getCCCIds();
+
+  const year = 74;
+  const receiving = receivingId;
+  const key = majorKey;
+
+  cccIds.forEach((id) => {
+    articulationParams.push({ year, id, receiving, key });
+  });
+
+  return articulationParams;
+}
+
 export async function getMajorData(receivingId) {
   try {
     const endpoint = `https://classglance.onrender.com/schools/major-data/${receivingId}/74`;
@@ -44,4 +84,23 @@ export async function getLowerDivs(receivingId, key) {
 
     return null;
   }
+}
+
+export async function getAllMajorArticulations(receivingId, majorKey) {
+  const receiving = receivingId;
+  const key = majorKey;
+
+  const articulationParams = await getArticulationParams(receiving, key);
+  const endpoint =
+    "https://classglance.onrender.com/articulations/articulation-params";
+
+  const formData = new FormData();
+  formData.append("parameters", articulationParams);
+
+  const res = await fetch(endpoint, {
+    method: "POST",
+    body: formData,
+  });
+
+  console.log(res.json());
 }

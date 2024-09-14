235e6bdfcb74f5aba9e57f642c6809d34798037f
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-09-14 01:55:10 -0700

    Remove old selecting functions

--- src/public/utils.js
+++ src/public/utils.js
@@ -51,61 +51,6 @@ export function alphaSort(array1, array2) {
   return arr;
 }
 
-export function selectClassByIndex(lowerDivsList, selectedIndex) {
-  lowerDivsList.forEach((item, index) => {
-    const lowerDiv = item;
-    const selected = true;
-
-    if (index === selectedIndex) {
-      if (Array.isArray(lowerDiv)) {
-        lowerDiv.push({ selected });
-      } else {
-        lowerDiv.selected = true;
-      }
-    }
-  });
-
-  sessionStorage.setItem("selectedLowerDivs", JSON.stringify(lowerDivsList));
-}
-
-export function deSelectAllClasses(lowerDivsList) {
-  lowerDivsList.forEach((item) => {
-    const lowerDiv = item;
-
-    if (Array.isArray(lowerDiv)) {
-      lowerDiv.forEach((subitem, subIndex) => {
-        if (typeof subitem === "object" && subitem.selected) {
-          lowerDiv.splice(subIndex, 1);
-        }
-      });
-    } else if (typeof lowerDiv === "object" && !Array.isArray(lowerDiv)) {
-      delete lowerDiv.selected;
-    }
-  });
-
-  sessionStorage.setItem("selectedLowerDivs", JSON.stringify(lowerDivsList));
-}
-
-export function getSelectedClass(allLowerDivs) {
-  let selectedClass;
-
-  allLowerDivs.forEach((lowerDiv) => {
-    if (Array.isArray(lowerDiv)) {
-      lowerDiv.forEach((item) => {
-        if (item.selected) {
-          selectedClass = lowerDiv.filter((subitem) => !subitem.selected);
-        }
-      });
-    } else if (typeof lowerDiv === "object" && lowerDiv.selected) {
-      const { selected, ...noSelected } = lowerDiv;
-
-      selectedClass = noSelected;
-    }
-  });
-
-  return selectedClass;
-}
-
 export function resetResults() {
   const results = document.querySelector(".results");
 

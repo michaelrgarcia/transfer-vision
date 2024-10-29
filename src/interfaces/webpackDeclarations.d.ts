d8256dfa429421802fda557789bddaf155a728e2
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-10-29 15:23:14 -0700

    Create for webpack asset imports

---
+++ src/interfaces/webpackDeclarations.d.ts
@@ -0,0 +1,19 @@
+declare module "*.woff" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.woff2" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}

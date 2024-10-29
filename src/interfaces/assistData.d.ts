6a05045f591ffbc2cd3e4adb25212ad8f59ef05e
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-10-29 12:50:13 -0700

    Finalize common type declarations

---
+++ src/interfaces/assistData.d.ts
@@ -0,0 +1,31 @@
+export interface LowerDiv {
+  prefix: string;
+  courseNumber: string; // some may be "22C" or "45L"
+  courseTitle: string;
+  courseId?: number; // series courses do not have a course ID
+  cid?: string; // c-ids are added later
+}
+
+interface SeriesIdObject {
+  seriesId: number;
+}
+
+interface ccNameObject {
+  ccName: string;
+}
+
+interface agreementLinkObject {
+  agreementLink: string;
+}
+
+export type UnfilteredSeries = (LowerDiv | string | SeriesIdObject)[];
+export type Series = (LowerDiv | string)[];
+
+type Articulation = LowerDiv | Series;
+
+export type FullArticulation = (
+  | Articulation
+  | string // connector like "and" or "or"
+  | ccNameObject
+  | agreementLinkObject
+)[];
